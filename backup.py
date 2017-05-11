cf8d41c3e487c0c066558156b279d21331ebaabd
linxiaobin <linxiaobin@felink.com>
2017-05-11 17:19:49 +0800

    保留当天每小时的备份以及1天后每天的1次备份

--- backup.py
+++ backup.py
@@ -6,7 +6,7 @@ from utility import *
 
 
 def backup(src, dst_dir, retemtion_days):
-    name = filename(src) + '_' + datetime.datetime.now().strftime('%Y%m%d' + fileExtension(src))
+    name = filename(src) + '_' + datetime.datetime.now().strftime('%Y%m%d%H' + fileExtension(src))
     dst = joinPaths(dst_dir, name)
     print('backup %s to %s' % (src, dst))
     createdir(dst_dir)
@@ -20,8 +20,14 @@ def backup(src, dst_dir, retemtion_days):
         name = filename(x)
         t = name.split('_')
         if t and len(t) > 1:
-            days = (datetime.datetime.now() - datetime.datetime.strptime(t[1], '%Y%m%d')).days
+            dt = datetime.datetime.strptime(t[1], '%Y%m%d%H')
+            days = (datetime.datetime.now() - dt).days
+            should_delete = False
             if days >= retemtion_days:
+                should_delete = True
+            elif days >= 1 and dt.hour >= 1:
+                should_delete = True
+            if should_delete:
                 file = joinPaths(dst_dir, x)
                 remove(file)
 
