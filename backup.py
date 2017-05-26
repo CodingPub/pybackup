eb80ad042553ff339d3a1b4f92874d1c0df3028a
linxiaobin <linxiaobin@felink.com>
2017-05-26 11:59:06 +0800

    优化备份文件数量

--- backup.py
+++ backup.py
@@ -5,7 +5,10 @@ import datetime
 from utility import *
 
 
-def backup(src, dst_dir, retemtion_days):
+def backup(src, dst_dir, retemtion_days, hours_last_day=None):
+    if hours_last_day is None:
+        hours_last_day = 8
+
     name = filename(src) + '_' + datetime.datetime.now().strftime('%Y%m%d%H' + fileExtension(src))
     dst = joinPaths(dst_dir, name)
     print('backup %s to %s' % (src, dst))
@@ -23,9 +26,11 @@ def backup(src, dst_dir, retemtion_days):
             dt = datetime.datetime.strptime(t[1], '%Y%m%d%H')
             days = (datetime.datetime.now() - dt).days
             should_delete = False
-            if days >= retemtion_days:
+            if days > 0 and days not in retemtion_days:
                 should_delete = True
             elif days >= 1 and dt.hour < 23:
+                should_delete = True
+            elif days == 0 and (datetime.datetime.now() - dt).seconds > hours_last_day * 60 * 60:
                 should_delete = True
             if should_delete:
                 file = joinPaths(dst_dir, x)
@@ -33,8 +38,13 @@ def backup(src, dst_dir, retemtion_days):
 
 
 if __name__ == '__main__':
+    # for day in range(0, 18):
+    #     for hour in range(0, 24):
+    #         path = '/tmp/backup/config_201705%02d%02d.json' % (26 - day, hour)
+    #         replacefile('/Users/linxiaobin/Developer/python/backup/config.json', path)
+
     content = readfile(joinPaths(cmddir(), 'config.json'))
     arr = str2Json(content)
     if arr and len(arr) > 0:
         for x in arr:
-            backup(x.get('src'), x.get('dst_dir'), x.get('retemtion_days'))
+            backup(x.get('src'), x.get('dst_dir'), x.get('retemtion_days'), hours_last_day=x.get('hours_last_day'))
