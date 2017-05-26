c89daa510142525f092f6cf8781198e47bbdab9c
linxiaobin <linxiaobin@felink.com>
2017-05-26 12:52:27 +0800

    fix bug

--- backup.py
+++ backup.py
@@ -26,11 +26,13 @@ def backup(src, dst_dir, retemtion_days, hours_last_day=None):
             dt = datetime.datetime.strptime(t[1], '%Y%m%d%H')
             days = (datetime.datetime.now() - dt).days
             should_delete = False
-            if days > 0 and days not in retemtion_days:
-                should_delete = True
-            elif days >= 1 and dt.hour < 23:
-                should_delete = True
-            elif days == 0 and (datetime.datetime.now() - dt).seconds > hours_last_day * 60 * 60:
+            if days >= 1:
+                if days in retemtion_days:
+                    if dt.hour < 23:
+                        should_delete = True
+                else:
+                    should_delete = True
+            elif days == 0 and dt.hour < 23 and (datetime.datetime.now() - dt).seconds > hours_last_day * 60 * 60:
                 should_delete = True
             if should_delete:
                 file = joinPaths(dst_dir, x)
@@ -38,10 +40,10 @@ def backup(src, dst_dir, retemtion_days, hours_last_day=None):
 
 
 if __name__ == '__main__':
-    # for day in range(0, 18):
-    #     for hour in range(0, 24):
-    #         path = '/tmp/backup/config_201705%02d%02d.json' % (26 - day, hour)
-    #         replacefile('/Users/linxiaobin/Developer/python/backup/config.json', path)
+    for day in range(0, 18):
+        for hour in range(0, 24):
+            path = '/tmp/backup/config_201705%02d%02d.json' % (26 - day, hour)
+            replacefile('/Users/linxiaobin/Developer/python/backup/config.json', path)
 
     content = readfile(joinPaths(cmddir(), 'config.json'))
     arr = str2Json(content)
